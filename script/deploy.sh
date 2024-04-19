428105705e41e56c07bb1d26e48ddd6a134cbab5
Mkw-k <rhauddn111@hanmail.net>
2024-04-19 15:46:07 +0900

    fix : 배포 스크립트 로그 추가

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,11 +1,17 @@
+#현재 위치 확인
+pwd
 # Git 저장소로 이동
 cd /home/ubuntu/SaveTime
+echo "위치 이동>>>>"
+pwd
 
 # 최신 변경 사항을 가져옴
 git pull origin master
+echo ">>>Git Pull Activate!!"
 
 # 변경된 어플리케이션 빌드
 ./gradlew clean build
+echo ">>>clean build Activate"
 
 # Spring Boot 어플리케이션 실행
 java -Dspring.profiles.active=dev -jar build/libs/SaveTime-0.0.1-SNAPSHOT.jar
