c5e4c7bc148896ed566a44a1cf0d0ba279be6e1f
Mkw-k <rhauddn111@hanmail.net>
2024-04-19 17:33:55 +0900

    fix : 스크립트 수정

--- script/deploy.sh
+++ script/deploy.sh
@@ -13,5 +13,7 @@ echo ">>>Git Pull Activate!!"
 ./gradlew clean bootjar
 echo ">>>clean build Activate"
 
+cd /build/libs
+
 # Spring Boot 어플리케이션 실행
-nohup java -Dspring.profiles.active=dev -jar build/libs/SaveTime-0.0.1-SNAPSHOT.jar &
+nohup java -Dspring.profiles.active=dev -jar SaveTime-0.0.1-SNAPSHOT.jar &
