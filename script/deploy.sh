51508cb305ee67db1fdd22949e5a0d68995020f8
Mkw-k <rhauddn111@hanmail.net>
2024-04-19 15:36:43 +0900

    fix : 배포 스크립트 수정

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,1 +1,11 @@
+# Git 저장소로 이동
+cd /path/to/your/repository
+
+# 최신 변경 사항을 가져옴
+git pull origin master
+
+# 변경된 어플리케이션 빌드
+./gradlew clean build
+
+# Spring Boot 어플리케이션 실행
 java -Dspring.profiles.active=dev -jar build/libs/SaveTime-0.0.1-SNAPSHOT.jar
