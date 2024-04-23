c65d6adb5dfdc625398bdd79845d308606bbcda1
Mkw-k <rhauddn111@hanmail.net>
2024-04-23 12:44:36 +0900

    fix : script chg 3

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,19 +1,29 @@
-#현재 위치 확인
-pwd
-# Git 저장소로 이동
-cd /home/ubuntu/SaveTime
-echo "위치 이동>>>>"
-pwd
+#!/bin/bash
+
+# 기존 Java 프로세스 중지
+echo "Stopping existing Java process..."
+existing_pid=$(ps -ef | grep "SaveTime-0.0.1-SNAPSHOT.jar" | grep -v grep | awk '{print $2}')
+if [ -n "$existing_pid" ]; then
+    echo "Killing existing process with PID: $existing_pid"
+    kill -9 $existing_pid
+    echo "Existing Java process stopped."
+else
+    echo "No existing Java process found."
+fi
 
 # 최신 변경 사항을 가져옴
+echo "Fetching latest changes from Git repository..."
+cd /home/ubuntu/SaveTime
 git pull origin master
-echo ">>>Git Pull Activate!!"
+echo "Git pull complete."
 
 # 변경된 어플리케이션 빌드
+echo "Building the application..."
 ./gradlew clean bootjar
-echo ">>>clean build Activate"
+echo "Build complete."
 
+# 새로운 프로젝트 실행
+echo "Starting the new Java process..."
 cd /home/ubuntu/SaveTime/build/libs
-
-# Spring Boot 어플리케이션 실행
-nohup java -Dspring.profiles.active=dev -jar SaveTime-0.0.1-SNAPSHOT.jar &
+nohup java -Dspring.profiles.active=dev -jar SaveTime-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
+echo "New Java process started."
